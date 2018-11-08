f58b71d98bfe9e2fa29d74105fb713b1c0d4f359
Sara Heljasvuo <sara.heljasvuo@heltti.fi>
2018-11-08 10:56:19 +0200

    Render LunchList

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,11 @@
 import * as React from 'react';
+import LunchList from './lunchList';
 
 class App extends React.Component {
   public render() {
     return (
       <div>
-          moi
+          <LunchList />
       </div>
     );
   }
