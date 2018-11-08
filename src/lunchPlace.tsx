8a1896e9e72838479ce56ae525765200fc67af90
Sara Heljasvuo <sara.heljasvuo@heltti.fi>
2018-11-08 13:21:48 +0200

    Remove address since we don't need it

--- src/lunchPlace.tsx
+++ src/lunchPlace.tsx
@@ -3,23 +3,17 @@ import { Component } from 'react';
 import './style.css';
 
 interface Props {
-    address: string;
-    menu: Array<string>;
+    menu: string;
     name: string;
 }
 
 export default class LunchPlace extends Component<Props> {
-    public render() {
-        const { address, menu, name, } = this.props;
+    render() {
+        const { menu, name, } = this.props;
         return (
             <div id="lunchList">
                 <h3 className="lunchPlaceTitle">{name}</h3>
-                <p className="lunchPlaceAddress">{address}</p>
-                <div className="lunchPlaceMenu">
-                    {menu.map((menuItem) =>
-                        <p key={menuItem}>{menuItem}</p>
-                    )}
-                </div>
+                <p className="lunchPlaceMenu">{menu}</p>
             </div>
         )
     }
