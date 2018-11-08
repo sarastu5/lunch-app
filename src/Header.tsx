3601f6460a49ee80f320a4b261912f2d28075377
Sara Heljasvuo <sara.heljasvuo@heltti.fi>
2018-11-08 13:20:39 +0200

    Title as a prop

--- src/Header.tsx
+++ src/Header.tsx
@@ -1,8 +1,8 @@
 import * as React from 'react';
 
-const Header = () => (
+const Header = (props: any) => (
     <div className="header">
-        <h1>Missä syödään?</h1>
+        <h1>{props.title}</h1>
     </div>
 )
 
